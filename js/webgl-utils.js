a9b7d6fd11363326c31de2a9c15e207f6b283ebc
geduldig <boxnumber03@gmail.com>
2014-04-19 09:05:06 -0400

    Update webgl-utils.js

--- js/webgl-utils.js
+++ js/webgl-utils.js
@@ -2,102 +2,103 @@ window.onerror = function(msg, url, lineno) {
 	alert(url + '(' + lineno + '): ' + msg);
 }
 
-function createShader(str, type) {
-	var shader = gl.createShader(type);
-	gl.shaderSource(shader, str);
-	gl.compileShader(shader);
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		throw gl.getShaderInfoLog(shader);
-	}
-	return shader;
-}
+function webglShader(_gl) {
+	var gl = _gl;
+	var my = {};
 
-function createProgram(vstr, fstr) {
-	var program = gl.createProgram();
-	var vshader = createShader(vstr, gl.VERTEX_SHADER);
-	var fshader = createShader(fstr, gl.FRAGMENT_SHADER);
-	gl.attachShader(program, vshader);
-	gl.attachShader(program, fshader);
-	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		throw gl.getProgramInfoLog(program);
+	function createShader(str, type) {
+		var shader = gl.createShader(type);
+		gl.shaderSource(shader, str);
+		gl.compileShader(shader);
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) 
+			throw gl.getShaderInfoLog(shader);
+		return shader;
 	}
-	return program;
-}
 
-function screenQuad() {
+	function linkProgram(program) {
+		var vshader = createShader(program.vshaderSource, gl.VERTEX_SHADER);
+		var fshader = createShader(program.fshaderSource, gl.FRAGMENT_SHADER);
+		gl.attachShader(program, vshader);
+		gl.attachShader(program, fshader);
+		gl.linkProgram(program);
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) 
+			throw gl.getProgramInfoLog(program);
+	}
+
+	function loadFile(file, callback, noCache, isJson) {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState == 1) {
+				if (isJson) 
+					request.overrideMimeType('application/json');
+				request.send();
+			} 
+			else if (request.readyState == 4) {
+				if (request.status == 200) 
+					callback(request.responseText);
+				else if (request.status == 404)
+					throw 'File "' + file + '" does not exist.';
+				else 
+					throw 'XHR error ' + request.status + '.';
+			}
+		};
+		var url = file;
+		if (noCache)
+			url += '?' + (new Date()).getTime();
+		request.open('GET', url, true);
+	}
+
+	my.loadProgram = function(vs, fs, callback) {
+		var program = gl.createProgram();
+		function vshaderLoaded(str) {
+			program.vshaderSource = str;
+			if (program.fshaderSource) {
+				linkProgram(program);
+				callback(program);
+			}
+		}
+		function fshaderLoaded(str) {
+			program.fshaderSource = str;
+			if (program.vshaderSource) {
+				linkProgram(program);
+				callback(program);
+			}
+		}
+		loadFile(vs, vshaderLoaded, true);
+		loadFile(fs, fshaderLoaded, true);
+		return program;
+	}
+
+	my.createProgram = function(vstr, fstr) {
+		var program = gl.createProgram();
+		var vshader = createShader(vstr, gl.VERTEX_SHADER);
+		var fshader = createShader(fstr, gl.FRAGMENT_SHADER);
+		gl.attachShader(program, vshader);
+		gl.attachShader(program, fshader);
+		gl.linkProgram(program);
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) 
+			throw gl.getProgramInfoLog(program);
+		return program;
+	};
+
+	return my;
+};
+
+function webglScreenQuad(gl) {
+	var vertices = [-1, -1, 1, -1, -1, 1, 1, 1];
 	var vertexPosBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
-	var vertices = [-1, -1, 1, -1, -1, 1, 1, 1];
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	vertexPosBuffer.itemSize = 2;
 	vertexPosBuffer.numItems = 4;
-
 	/*
 	 2___3
-	 |\  |
+	|\  |
 	 | \ |
-	 |__\|
-	 0   1
+	|__\|
+	0   1
 	*/
 	return vertexPosBuffer;
-}
-
-function linkProgram(program) {
-	var vshader = createShader(program.vshaderSource, gl.VERTEX_SHADER);
-	var fshader = createShader(program.fshaderSource, gl.FRAGMENT_SHADER);
-	gl.attachShader(program, vshader);
-	gl.attachShader(program, fshader);
-	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		throw gl.getProgramInfoLog(program);
-	}
-}
-
-function loadFile(file, callback, noCache, isJson) {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if (request.readyState == 1) {
-			if (isJson) {
-				request.overrideMimeType('application/json');
-			}
-			request.send();
-		} else if (request.readyState == 4) {
-			if (request.status == 200) {
-				callback(request.responseText);
-			} else if (request.status == 404) {
-				throw 'File "' + file + '" does not exist.';
-			} else {
-				throw 'XHR error ' + request.status + '.';
-			}
-		}
-	};
-	var url = file;
-	if (noCache) {
-		url += '?' + (new Date()).getTime();
-	}
-	request.open('GET', url, true);
-}
-
-function loadProgram(vs, fs, callback) {
-	var program = gl.createProgram();
-	function vshaderLoaded(str) {
-		program.vshaderSource = str;
-		if (program.fshaderSource) {
-			linkProgram(program);
-			callback(program);
-		}
-	}
-	function fshaderLoaded(str) {
-		program.fshaderSource = str;
-		if (program.vshaderSource) {
-			linkProgram(program);
-			callback(program);
-		}
-	}
-	loadFile(vs, vshaderLoaded, true);
-	loadFile(fs, fshaderLoaded, true);
-	return program;
 }
 
 (function() {
@@ -124,3 +125,4 @@ function loadProgram(vs, fs, callback) {
             clearTimeout(id);
         };
 }())
+
