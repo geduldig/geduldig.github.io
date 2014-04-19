4e7c59d24027a4010b15b29caae7ff2f56006b9b
geduldig <boxnumber03@gmail.com>
2014-04-19 09:05:49 -0400

    Create video-utils.js

---
+++ js/video-utils.js
@@ -0,0 +1,20 @@
+function setupCamera(video, callback) {
+    navigator.getUserMedia = navigator.getUserMedia || 
+                             navigator.webkitGetUserMedia || 
+                             navigator.mozGetUserMedia || 
+                             navigator.msGetUserMedia;
+
+	function onMediaStream(stream) {
+	    window.URL = window.URL || window.webkitURL;
+		video.src = window.URL.createObjectURL(stream);
+		video.onloadedmetadata = function(e) { 
+			callback(null, stream);
+		};
+	}
+
+	function onMediaFail(e) {
+		callback(e, null);
+	}
+
+    navigator.getUserMedia({video:true}, onMediaStream, onMediaFail);
+}
